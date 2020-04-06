d0bbfe46dbc37d4cc84fc7ee1e7c463fab137d6a
rutto <emmanuelrutto06@gmail.com>
2020-04-06 13:08:31 +0300

    extra changes

--- library.py
+++ library.py
@@ -11,7 +11,7 @@ class Library:
         self.root = root
         self.root.title("Rutto's Library Mangaement System")
         self.root.geometry("1350x750+0+0")
-        self.root.configure(background='Purple')
+        self.root.configure(background='Blue')
 
         #================================================FRAME==============================================================================================================
 
@@ -54,13 +54,13 @@ class Library:
         self.txtBookID .grid(row=0, column=3)
 
 
-        self.lblBookTitle=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Book ID:', padx=2, pady=2)
+        self.lblBookTitle=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Book Title:', padx=2, pady=2)
         self.lblBookTitle .grid(row=1, column=2, sticky=W)
-        self.lblBookTitle = Entry(DataFrameLEFT, font=('arial', 12, 'bold'), width =25)
-        self.lblBookTitle.grid(row=1, column=3)
+        self.txtBookTitle = Entry(DataFrameLEFT, font=('arial', 12, 'bold'), width =25)
+        self.txtBookTitle.grid(row=1, column=3)
 
 
-        self.lblRef=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Book ID:', padx=2, pady=2)
+        self.lblRef=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Reference NO:', padx=2, pady=2)
         self.lblRef .grid(row=1, column=0, sticky=W)
         self.txtRef= Entry(DataFrameLEFT, font=('arial', 12, 'bold'), width =25)
         self.txtRef .grid(row=1, column=1)
@@ -74,20 +74,76 @@ class Library:
        	self.cboTitle.current(0)
        	self.cboTitle.grid(row=2, column=1)
 
-        self.lblAuthor=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Book ID:', padx=2, pady=2)
+        self.lblAuthor=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Author:', padx=2, pady=2)
         self.lblAuthor .grid(row=2, column=2, sticky=W)
         self.txtAuthor= Entry(DataFrameLEFT, font=('arial', 12, 'bold'), width =25)
         self.txtAuthor .grid(row=2, column=3)
 
-        self.lblFirstname=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Book ID:', padx=2, pady=2)
+        self.lblFirstname=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='First Name:', padx=2, pady=2)
         self.lblFirstname .grid(row=3, column=0, sticky=W)
         self.txtFirstname= Entry(DataFrameLEFT, font=('arial', 12, 'bold'), width =25)
         self.txtFirstname .grid(row=3, column=1)
 
-        self.lblDateBorrowed=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Book ID:', padx=2, pady=2)
+        self.lblDateBorrowed=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Date Borrowed:', padx=2, pady=2)
         self.lblDateBorrowed .grid(row=3, column=2, sticky=W)
         self.txtDateBorrowed= Entry(DataFrameLEFT, font=('arial', 12, 'bold'), width =25)
         self.txtDateBorrowed .grid(row=3, column=3)
+
+        self.lblSurname=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Surname:', padx=2, pady=2)
+        self.lblSurname .grid(row=4, column=0, sticky=W)
+        self.txtSurname= Entry(DataFrameLEFT, font=('arial', 12, 'bold'), width =25)
+        self.txtSurname .grid(row=4, column=1)
+
+        self.lblDateDue=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Date Due:', padx=2, pady=6)
+        self.lblDateDue .grid(row=4, column=2, sticky=W)
+        self.txtDateDue= Entry(DataFrameLEFT, font=('arial', 12, 'bold'), width =25)
+        self.txtDateDue .grid(row=4, column=3)
+
+        self.lblAddress1=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Address 1:', padx=2, pady=2)
+        self.lblAddress1 .grid(row=5, column=0, sticky=W)
+        self.txtAddress1= Entry(DataFrameLEFT, font=('arial', 12, 'bold'), width =25)
+        self.txtAddress1 .grid(row=5, column=1)
+
+        self.lblDaysOnLoan=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Days On Loan:', padx=2, pady=2)
+        self.lblDaysOnLoan .grid(row=5, column=2, sticky=W)
+        self.txtDaysOnLoan= Entry(DataFrameLEFT, font=('arial', 12, 'bold'), width =25)
+        self.txtDaysOnLoan .grid(row=5, column=3)
+
+        self.lblAddress2=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Address 2:', padx=2, pady=2)
+        self.lblAddress2 .grid(row=6, column=0, sticky=W)
+        self.txtAddress2= Entry(DataFrameLEFT, font=('arial', 12, 'bold'), width =25)
+        self.txtAddress2 .grid(row=6, column=1)
+
+        self.lblLateReturnFine=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text=':Date Return Fine', padx=2, pady=2)
+        self.lblLateReturnFine .grid(row=6, column=2, sticky=W)
+        self.txtLateReturnFine= Entry(DataFrameLEFT, font=('arial', 12, 'bold'), width =25)
+        self.txtLateReturnFine .grid(row=6, column=3)
+
+        self.lblPostCode=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Post Code:', padx=2, pady=2)
+        self.lblPostCode .grid(row=7, column=0, sticky=W)
+        self.txtPostCode= Entry(DataFrameLEFT, font=('arial', 12, 'bold'), width =25)
+        self.txtPostCode .grid(row=7, column=1)
+
+        self.lblDateOverDue=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Date Over Due:', padx=2, pady=2)
+        self.lblDateOverDue .grid(row=7, column=2, sticky=W)
+        self.txtDateOverDue= Entry(DataFrameLEFT, font=('arial', 12, 'bold'), width =25)
+        self.txtDateOverDue .grid(row=7, column=3)
+
+        self.lblMobileNo=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Mobile NO:', padx=2, pady=2)
+        self.lblMobileNo .grid(row=8, column=0, sticky=W)
+        self.txtMobileNO= Entry(DataFrameLEFT, font=('arial', 12, 'bold'), width =25)
+        self.txtMobileNO .grid(row=8, column=1)
+
+        self.lblSellingPrice=Label(DataFrameLEFT, font=('arial', 12, 'bold',), text='Selling Price:', padx=2, pady=2)
+        self.lblSellingPrice .grid(row=8, column=2, sticky=W)
+        self.txtSellingPrice= Entry(DataFrameLEFT, font=('arial', 12, 'bold'), width =25)
+        self.txtSellingPrice .grid(row=8, column=3)
+        #=============================================================Button=====================================================================
+    	self.btnDisplayData=Button(ButtonFrame, text='DisplayData',font=('arial', 12, 'bold'), width=30, bd=4)
+   		self.btnDisplayData.grid(row=0,column=0)
+
+
+
 
 
 if __name__ =='__main__':
